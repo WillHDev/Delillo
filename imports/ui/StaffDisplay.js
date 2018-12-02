c7979a55d6633663aa5c7e98a0c0bc166c8edeab
William Holcombe <sphinxelite@Williams-iMac.local>
2018-12-02 18:27:03 -0500

    refined animation of staff photos

--- imports/ui/StaffDisplay.js
+++ imports/ui/StaffDisplay.js
@@ -3,40 +3,42 @@ import { fadeInUp } from 'react-animations';
 import styled, { keyframes } from 'styled-components';
 import './StaffDisplay.css'
 import posed from 'react-pose';
-
+import MasonryLayout from 'react-masonry-layout';
 
 const fadeInUpAnimation = keyframes`${fadeInUp}`;
 
 const FadeInUpDiv = styled.div`
 animation: 1s ${fadeInUpAnimation};
 `;
-const StaggerFade = posed.ul({
-    open: {
-        x: '0%',
-        delayChildren: 200,
-        staggerChildren: 50
-    },
-    closed: { x: '-100%', delay: 300 },
-    initialPose: 'closed'
-})
+// const StaggerFade = posed.ul({
+//     open: {
+//         x: '0%',
+//         delayChildren: 200,
+//         staggerChildren: 50
+//     },
+//     closed: { x: '-100%', delay: 300 },
+//     initialPose: 'closed'
+// })
 
 export default class StaffDisplay extends Component {
     render() {
         return (
             <div className="staff-display-container">
-
-                {this.props.staff.map(staff => {
-                    console.log('staff', staff);
-                    return (
-                        <FadeInUpDiv>
-                            <li key={staff._id}>
-                                <h4> {staff.name}</h4>
-                                <img width="200" src={staff.img} alt={staff.img} />
-                            </li>
-                        </FadeInUpDiv>
-                    )
-                })}
-
+                <ul>
+                    <MasonryLayout id="masonry-layout">
+                        {this.props.staff.map((staff, i) => {
+                            console.log('staff', staff);
+                            return (
+                                <FadeInUpDiv key={staff._id}>
+                                    <div key={staff._id}>
+                                        <h4> {staff.name}</h4>
+                                        <img width="200" src={staff.img} alt={staff.img} />
+                                    </div>
+                                </FadeInUpDiv>
+                            )
+                        })}
+                    </MasonryLayout>
+                </ul>
             </div>
         )
     }
